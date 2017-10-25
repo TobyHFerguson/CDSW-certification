c64f2c4b125af3b57ae1a44c78b3a46833c4dc61
Toby Ferguson <toby@cloudera.com>
2017-10-25 23:50:32 +0000

    Minor improvements
    moved items around
    Added the geom_count() graph

--- scripts/explore.R
+++ scripts/explore.R
@@ -16,10 +16,15 @@ brfss <- brfss_2011
 
 # Filter out the missing or unknown asthmatic responses
 
-brfss <- brfss %>% filter(CASTHM1 %in% c(1,2)) %>% mutate(hasasthma=CASTHM1 - 1) %>% mutate(smoker = if_else(SMOKER3 == 1, 'everyday', if_else(SMOKER3 == 2, 'somedays', if_else(SMOKER3 == 3, 'former', if_else(SMOKER3 == 4, 'never', 'unknown' )))))
+brfss <- brfss %>% filter(CASTHM1 %in% c(1,2)) %>% 
+  mutate(hasasthma=CASTHM1 - 1)
 
 
-brfss_smoking <- select(brfss, smoker, hasasthma) %>% collect %>% mutate_all(funs(as.factor(.)))
+brfss_smoking <- brfss  %>%
+  select(SMOKER3, hasasthma) %>% 
+  collect %>% 
+  mutate(smoker = if_else(SMOKER3 == 1, 'everyday', if_else(SMOKER3 == 2, 'somedays', if_else(SMOKER3 == 3, 'former', if_else(SMOKER3 == 4, 'never', 'unknown' ))))) %>%
+  mutate_all(funs(as.factor(.)))
 
 # This draws a basic count plot:
 brfss_smoking %>% ggplot(aes(x=smoker)) + geom_bar()
@@ -28,7 +33,7 @@ brfss_smoking %>% ggplot(aes(x=smoker)) + geom_bar()
 # * shows the density for each label against the asthmatic and non-asthmatic population
 
 
-p <- brfss_smoking %>% ggplot(aes(x=smoker, y=..count../sum(..count..), group=hasasthma)) + labs(y="density") + geom_bar(aes(y=..count../sum(..count..),fill=hasasthma))
+p <- brfss_smoking %>% ggplot(aes(x=smoker, y=(..count../sum(..count..)*100))) + labs(y="prevalence") + geom_bar(aes(fill=hasasthma))
 
 p
 
@@ -37,5 +42,29 @@ p
 p + stat_count(geom="text", 
                colour="black", 
                size=3.5,
-               aes(x=smoker, label=round(..count../sum(..count..), 2), group=hasasthma), position=position_stack(vjust=0.5))
+               aes(x=smoker, 
+                   label=paste0(round(..count../sum(..count..)*100, 2), "%"), group=hasasthma), 
+                   position=position_stack(vjust=0.5)) +
+    stat_count(geom="text", 
+                   colour="blue", 
+                   size=3.5,
+                   aes(x=smoker, 
+                       label=paste0(round(..count../sum(..count..)*100, 2), "%")))
+# What we can see here is that smoking occurs (apart from the 'somedays' smokers)
+a <- aes(x,y,label=n)
+c <- brfss_smoking %>% ggplot() + geom_count(aes(x=smoker, y=hasasthma,color=..n.., size=..n..))
+c <- c + guides(color="legend")
+c+ geom_text(data = ggplot_build(c)$data[[1]], 
+              a, nudge_y=0.1, color = "black")
+c <- c + stat_count(geom="text", 
+               colour="black", 
+               size=3.5,
+               aes(x=smoker, 
+                   label=paste0(round(..count../sum(..count..)*100, 2), "%"), group=hasasthma), 
+                   position=position_stack(vjust=0.5))
+c
+
+# What I want to do is to build a function that will produce these kind of graphs for my various categorical
+# attributes.
+
 
